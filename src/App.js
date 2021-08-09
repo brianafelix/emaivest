b98c2a341e90982b73c24e504d2c4280b1678a21
marcio <marcioqui3@gmail.com>
2021-08-09 12:48:28 +0100

    novo update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { useState } from 'react';
-import Navbar from './components/navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';
-import Main from './components/main/Main';
+import { useState } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/main/Main";
 
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
-const App=() =>{
- 
- const[sidebarOpen,setsidebarOpen]=useState(false)
- const OpenSidebar =() =>{
-   setsidebarOpen(true);
- };
- const closeSidebar=() =>{
-   setsidebarOpen(false);
- }
- 
+const App = () => {
+  const [inactive, setInactive] = useState(false);
+
   return (
-    <div className="container">
-      <Navbar sidebarOpen={sidebarOpen} OpenSidebar={OpenSidebar}/>
-      <Main  SidebarOpen={sidebarOpen} closeSideba={closeSidebar}/>
-     <Sidebar SidebarOpen={sidebarOpen} closeSidebar={closeSidebar}/>
-    
-    </div>
+    <>
+      {" "}
+      <Sidebar
+        onCollapse={(inactive) => {
+          setInactive(inactive);
+        }}
+      />
+      <div className={`s-container ${inactive ? "inactive" : ""}`}>
+        <Navbar />
+        <Main />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,78 +1,88 @@
-import'./Sidebar.css';
-import logo from '../../assets/logo.jgp.png';
-const Sidebar= ({SidebarOpen, closeSidebar }) =>{
+import "./Sidebar.css";
+import React, { useContext, useEffect, useState } from "react";
+import logo from "../../assets/logo.jgp.png";
+const Sidebar = (props) => {
+  const [inactive, setInactive] = useState(false);
 
-    return(
-        <div className={SidebarOpen?"Sidebar-responsive" :""}  id="Sidebar">
-        <div className="Sidebar__title">
-            <div className="Sidebar__img">
-                <img src={logo} alt="logo"/>
-                <h1>EMARPE Soluçoes</h1>
-            </div>
-            <i 
-            onClick={() => closeSidebar()} 
-            className="bi bi-x-square-fill"
-            id="SidebarIcon"
-            aria-hidden="true"
-            ></i>
+  useEffect(() => {
+    props.onCollapse(inactive);
+  }, [inactive]);
+
+  return (
+    <div className={inactive ? "sidebar-inactive" : ""} id="Sidebar">
+      <div className="Sidebar__title">
+        <div className="Sidebar__img">
+          <img
+            src={logo}
+            alt="logo"
+            onClick={() => {
+              setInactive(!inactive);
+            }}
+          />
+          <h1>EMARPE Soluçoes</h1>
         </div>
-        <div className="Sidebar__menu">
+        <i
+          className="bi bi-x-square-fill"
+          id="SidebarIcon"
+          aria-hidden="true"
+        ></i>
+      </div>
+      <div className="Sidebar__menu">
         <div className="Sidebar__link active_menu_link">
-        <i class="bi bi-list"></i>
-         <a href="#">home</a>
-            </div>
-            <h2>ADMIN</h2>
-            <div className="Sidebar__link">
-            <i class="bi bi-speedometer"></i>
-         <a href="#">Area administrativa</a></div>
+          <i class="bi bi-list"></i>
+          <a href="#">home</a>
+        </div>
+        <div className="Sidebar__link">
+          <i class="bi bi-speedometer"></i>
+          <a href="#">Area administrativa</a>
+        </div>
 
-         <div className="Sidebar__link">
-            <i className="bi bi-shop"></i>
-         <a href="#">lojas</a></div>
+        <div className="Sidebar__link">
+          <i className="bi bi-shop"></i>
+          <a href="#">lojas</a>
+        </div>
 
-         <div className="Sidebar__link">
-         <i class="bi bi-bag"></i>
-         <a href="#">produtos</a></div>
+        <div className="Sidebar__link">
+          <i class="bi bi-bag"></i>
+          <a href="#">produtos</a>
+        </div>
 
-         <div className="Sidebar__link">
-         <i class="bi bi-justify"></i>  
-         <a href="#">categorias</a></div>
+        <div className="Sidebar__link">
+          <i class="bi bi-justify"></i>
+          <a href="#">categorias</a>
+        </div>
 
-         <div className="Sidebar__link">
-            <i className="fa fa-cutlery"></i>
-         <a href="#">pedidos</a></div>
-                  <h2>Pessoas</h2>
-           <div className="Sidebar__link">
-           <i class="bi bi-person-fill"></i> 
-         <a href="#">Administrador</a></div>
+        <div className="Sidebar__link">
+          <i class="bi bi-person-fill"></i>
+          <a href="#">Administrador</a>
+        </div>
 
-         <div className="Sidebar__link">
-         <i class="bi bi-people-fill"></i>
-         <a href="#">Usuarios</a></div>
+        <div className="Sidebar__link">
+          <i class="bi bi-people-fill"></i>
+          <a href="#">Usuarios</a>
+        </div>
 
+        <div className="Sidebar__link">
+          <i class="bi bi-currency-exchange"></i>
+          <a href="#">Pagamentos E Custos </a>
+        </div>
 
-         <div className="Sidebar__link">
-         <i class="bi bi-currency-exchange"></i>
-         <a href="#">Pagamentos E Custos </a></div>
+        <div className="Sidebar__link">
+          <i class="bi bi-list"></i>
+          <a href="#">A Plataforma </a>
+        </div>
 
+        <div className="Sidebar__link">
+          <i class="bi bi-file-earmark-text"></i>
+          <a href="#">politica de privacidade</a>
+        </div>
 
-         <div className="Sidebar__link">
-         <i class="bi bi-list"></i>
-         <a href="#">A Plataforma </a></div>
-
-
-
-         <div className="Sidebar__link">
-         <i class="bi bi-file-earmark-text"></i>
-         <a href="#">politica de privacidade</a></div>
-
-
-         <div className="Sidebar__logout">
-         <i class="bi bi-toggle-off"></i>
-         <a href="#"> LOG OUT</a></div>
-
-  </div>
-  </div>
-    )
-}
+        <div className="Sidebar__logout">
+          <i class="bi bi-power"></i>
+          <a href="#"> LOG OUT</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Sidebar;
